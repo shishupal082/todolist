849aed26836699307b5b6d123fb17c81968eb840
Shishupal Kumar <shishupal.kumar@hashedin.com>
2012-12-29 01:37:02 +0530

    add or assign task to different collaborators and fotters

--- static/js/todolist.js
+++ static/js/todolist.js
@@ -1,5 +1,38 @@
-
-function myFunction()
+function mynewFunction1(el)
 {
-alert("Hello World!");
+       var x=el.value;
+       var y=document.createElement("li");
+       y.innerHTML = x;
+       $("#collaborator1").append(y);
+}
+
+
+function mynewFunction2(el)
+{
+       var x=el.value;
+       var y=document.createElement("li");
+       y.innerHTML = x;
+       $("#collaborator2").append(y);
+}
+
+function mynewFunction3(el)
+{
+       var x=el.value;
+       var y=document.createElement("li");
+       y.innerHTML = x;
+       $("#collaborator3").append(y);
+}
+function BACKLOG(el)
+{
+       var x=el.value;
+       var y=document.createElement("li");
+       y.innerHTML = x;
+       $("#BACKLOG").append(y);
+}
+function DONE_ITEMS(el)
+{
+       var x=el.value;
+       var y=document.createElement("li");
+       y.innerHTML = x;
+       $("#DONE_ITEMS").append(y);
 }
